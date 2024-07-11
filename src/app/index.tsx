e93e23318fdf2bac2cd83cb4107b34acec3989ba
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-07-11 19:13:04 -0300

    feat: configure Expo fonts and add loading

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,10 @@ import { Text, View } from 'react-native';
 
 export default function Index() {
 	return (
-		<View>
-			<Text>Hello!</Text>
+		<View className='flex-1 items-center justify-center'>
+			<Text className='text-zinc-400'>
+				Convide seus amigos e planeje sua próxima viagem!
+			</Text>
 		</View>
 	);
 }
